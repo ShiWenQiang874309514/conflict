f1048a5f81eccd5d6377952dc39b80433dd312df
swq <swq@qq.com>
2020-05-05 08:28:11 +0800

    首次创建spring initializr

---
+++ src/test/java/com/swq/mvn/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swq.mvn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
